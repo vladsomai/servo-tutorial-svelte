15ae56c6604f2da84158607003634d56957b9a86
Vlad Somai <Vlad.Somai-3rdpty@brooksautomation.com>
2025-08-16 19:21:57 +0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Gearotons Home page" />
+</svelte:head>
+
+<section>
+	<h1>Welcome</h1>
+</section>
