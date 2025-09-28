c7676cb148daa53e5237e9ce897dd541013a84ba
Vlad Somai <Vlad.Somai-3rdpty@brooksautomation.com>
2025-09-28 16:34:42 +0300

    add search for cmds and fix icons to use dynamic stroke

--- src/lib/components/command-menu.svelte
+++ src/lib/components/command-menu.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import type { MotorCommandType } from '$lib/client-server-lib/types';
 	import LogoImg from '$lib/images/logo.png';
 	import Icon from '@iconify/svelte';
 
@@ -10,12 +11,35 @@
 	let isAtleastOneExpanded: boolean = $derived.by(() => {
 		return menuGroupsExpanded.includes(true);
 	});
+
+	let searchText = $state('');
+
+	let motorCommandsGroupedFiltered: Map<string, number[]> = $state(
+		new Map(data.MotorCommandsGrouped)
+	);
+
+	$effect(() => {
+		const tempCmds = new Map();
+		for (const [group, commandsIds] of data.MotorCommandsGrouped) {
+			const filteredCmdPerGroup = commandsIds
+				.map((cmdid: number) => data.MotorCommands.get(cmdid))
+				.filter((cmd: MotorCommandType) => {
+					if (cmd != null)
+						return cmd.CommandString.toLowerCase().includes(searchText.toLocaleLowerCase());
+				});
+			const cmdIdsFiltered = filteredCmdPerGroup.map((cmd: MotorCommandType) => cmd.CommandEnum);
+			tempCmds.set(group, cmdIdsFiltered);
+		}
+		motorCommandsGroupedFiltered = tempCmds;
+	});
 </script>
 
 <div class="flex flex-col justify-center py-10">
 	<a href="/" class="mx-auto">
 		<img src={LogoImg} height="auto" width="160" alt="Gearotons" />
 	</a>
+
+	<input bind:value={searchText} class="input input-sm my-3" placeholder="Search command" />
 
 	<button
 		title={'Collapse all'}
@@ -35,7 +59,7 @@
 		{/if}
 	</button>
 	<ul class="menu bg-base-200 rounded-box w-full">
-		{#each data.MotorCommandsGrouped as [key, value], index}
+		{#each motorCommandsGroupedFiltered as [key, value], index}
 			<li>
 				<details bind:open={menuGroupsExpanded[index]}>
 					<summary class="opacity-50">{key}</summary>
@@ -44,7 +68,7 @@
 							<li class="mt-3">
 								<a
 									href={`/docs/${cmdid}`}
-									class={`hover:bg-base-300 hover:text-primary ${cmdid == data.CommandId ? 'bg-base-300 text-primary' : ''} w-full p-3 text-left text-sm rounded-l-lg rounded-r-full`}
+									class={`hover:bg-base-300 hover:text-primary ${cmdid == data.CommandId ? 'bg-base-300 text-primary' : ''} w-full rounded-l-lg rounded-r-full p-3 text-left text-sm`}
 									>{data.MotorCommands.get(cmdid).CommandString}</a
 								>
 							</li>
--- src/lib/components/commands/command.svelte
+++ src/lib/components/commands/command.svelte
@@ -20,17 +20,33 @@
 
 <div class="flex w-full justify-between">
 	<button
-		class="btn rounded-full px-3 text-black"
+		class="btn rounded-full px-3"
 		onclick={() => {
 			SetModalComponent(Model3d);
 			Modal.Dialog?.showModal();
 		}}
 	>
-		<img src={View3dIcon} alt="info" width="25" height="auto" />
+		<svg
+			class="stroke-base-content"
+			width="20px"
+			height="20px"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M12 10.2308L3.08495 7.02346M12 10.2308L20.9178 7.03406M12 10.2308V20.8791M5.13498 18.5771L10.935 20.6242C11.3297 20.7635 11.527 20.8331 11.7294 20.8608C11.909 20.8853 12.091 20.8853 12.2706 20.8608C12.473 20.8331 12.6703 20.7635 13.065 20.6242L18.865 18.5771C19.6337 18.3058 20.018 18.1702 20.3018 17.9269C20.5523 17.7121 20.7459 17.4386 20.8651 17.1308C21 16.7823 21 16.3747 21 15.5595V8.44058C21 7.62542 21 7.21785 20.8651 6.86935C20.7459 6.56155 20.5523 6.28804 20.3018 6.0732C20.018 5.82996 19.6337 5.69431 18.865 5.42301L13.065 3.37595C12.6703 3.23665 12.473 3.167 12.2706 3.13936C12.091 3.11484 11.909 3.11484 11.7294 3.13936C11.527 3.167 11.3297 3.23665 10.935 3.37595L5.13498 5.42301C4.36629 5.69431 3.98195 5.82996 3.69824 6.0732C3.44766 6.28804 3.25414 6.56155 3.13495 6.86935C3 7.21785 3 7.62542 3 8.44058V15.5595C3 16.3747 3 16.7823 3.13495 17.1308C3.25414 17.4386 3.44766 17.7121 3.69824 17.9269C3.98195 18.1702 4.36629 18.3058 5.13498 18.5771Z"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+
 		View 3D</button
 	>
 	{#if currentCommand != null && CommandsWithShortcuts.get(currentCommand?.CommandEnum) != null}
 		<button
+			aria-labelledby="info"
 			class="btn m-0 rounded-full px-2"
 			onclick={() => {
 				SetModalContent({
@@ -42,11 +58,33 @@
 				Modal.Dialog?.showModal();
 			}}
 		>
-			<img src={InfoIcon} alt="info" width="25" height="auto" />
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="25"
+				height="25"
+				fill="currentColor"
+				class="bi bi-info-circle-fill"
+				viewBox="0 0 16 16"
+			>
+				<path
+					d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"
+				/>
+			</svg>
 		</button>
 	{/if}
-	<a href="/feedback" class="btn rounded-full text-black">
-		<img src={FeedbackIcon} alt="feedback" />
+	<a href="/feedback" class="btn rounded-full">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="18"
+			height="18"
+			fill="currentColor"
+			class="bi bi-envelope-paper stroke-base-content"
+			viewBox="0 0 16 16"
+		>
+			<path
+				d="M4 0a2 2 0 0 0-2 2v1.133l-.941.502A2 2 0 0 0 0 5.4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V5.4a2 2 0 0 0-1.059-1.765L14 3.133V2a2 2 0 0 0-2-2H4Zm10 4.267.47.25A1 1 0 0 1 15 5.4v.817l-1 .6v-2.55Zm-1 3.15-3.75 2.25L8 8.917l-1.25.75L3 7.417V2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v5.417Zm-11-.6-1-.6V5.4a1 1 0 0 1 .53-.882L2 4.267v2.55Zm13 .566v5.734l-4.778-2.867L15 7.383Zm-.035 6.88A1 1 0 0 1 14 15H2a1 1 0 0 1-.965-.738L8 10.083l6.965 4.18ZM1 13.116V7.383l4.778 2.867L1 13.117Z"
+			/>
+		</svg>
 		Feedback</a
 	>
 </div>
