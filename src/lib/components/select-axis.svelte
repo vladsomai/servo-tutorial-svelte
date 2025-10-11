5b9ef4d03125dc80139be9a1c3717aee8fc9ace7
Vlad Somai <Vlad.Somai-3rdpty@brooksautomation.com>
2025-10-11 20:35:16 +0300

    make the selected axis responsive

--- src/lib/components/select-axis.svelte
+++ src/lib/components/select-axis.svelte
@@ -2,24 +2,31 @@
 	import { SelectedAxis, SelectedUniqueID } from '$lib/stores/global';
 	import { fly } from 'svelte/transition';
 	import LabeledInput from './labeled-input.svelte';
+
+	let inputs = [
+		{ label: 'Alias', bindable: $SelectedAxis },
+		{ label: 'UniqueID', bindable: $SelectedUniqueID }
+	];
 </script>
 
-<LabeledInput
-	Class="mt-2"
-	TooltipText={''}
-	Label={'Alias'}
-	bind:InputValue={$SelectedAxis}
-	Type="string"
-/>
+<div class="flex flex-col lg:flex-row">
+	<LabeledInput
+		Class="mt-2"
+		TooltipText={''}
+		Label={'Alias'}
+		bind:InputValue={$SelectedAxis}
+		Type="string"
+	/>
 
-{#if $SelectedAxis == '254'}
-	<div transition:fly={{ duration: 1000, y: -30 }}>
-		<LabeledInput
-			Class="mt-2"
-			TooltipText={''}
-			Label={'UniqueID'}
-			bind:InputValue={$SelectedUniqueID}
-			Type="string"
-		/>
-	</div>
-{/if}
+	{#if $SelectedAxis == '254'}
+		<div transition:fly={{ duration: 1000, y: -30 }}>
+			<LabeledInput
+				Class="mt-2 lg:ml-2"
+				TooltipText={''}
+				Label={'UniqueID'}
+				bind:InputValue={$SelectedUniqueID}
+				Type="string"
+			/>
+		</div>
+	{/if}
+</div>
